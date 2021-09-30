639e3b11f45d811f4e2374a7475d2c405afca741
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-09-29 23:31:45 -0300

    update file name, add process env port  and Procfile

--- src/app.js
+++ src/app.js
@@ -41,5 +41,5 @@ app.get('/', (req, res) => {
 });
 
 // start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server running http://localhost:${port}`));
